0dbb650eb27dfedc6a8cb9b4dc42b4aa08c95ce3
EljonasCavalcante <eljonascavalcante@hotmail.com>
2022-04-11 13:36:28 -0300

    perf: updated code to improve performance and implementation of services and code cleanup

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
     nome: {
       type: DataTypes.STRING,
       validate: {
         funcaoValidadora: function(dado) {
-          if (dado.length < 3) throw new Error('o campo nome deve ter mais de três caracteres')
+          if (dado.length < 3) throw new Error('o campo nome deve ter mais de 3 caracteres')
         }
       }
     },
@@ -15,21 +15,20 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         isEmail: {
           args: true,
-          msg: 'dado do tipo e-mail inválidos'
+          msg: 'dado do tipo e-mail inválido'
         }
       }
-   },
+    },
     role: DataTypes.STRING
   }, { 
     paranoid: true,
     defaultScope: {
-        where: { ativo: true}
-    },
+      where: { ativo: true }
+    },  
     scopes: {
-      todos : { where: {} },
-      //etc: { constraint: valor}
+      todos: { where: {} },
     }
-  });
+  })
   Pessoas.associate = function(models) {
     Pessoas.hasMany(models.Turmas, {
       foreignKey: 'docente_id'
@@ -41,4 +40,4 @@ module.exports = (sequelize, DataTypes) => {
     })
   }
   return Pessoas
-};
+}
